4bfc54f65ae0f691f9da8322060dd517da73c3a2
Priscilla Wang <hjkim015@users.noreply.github.com>
2020-05-16 19:18:29 -0700

    began pattern function so it shows the unique positions of repeated letters in each encoded word

--- cipher/codebreaker.py
+++ cipher/codebreaker.py
@@ -1,11 +1,12 @@
 
 from nltk.corpus import words
+import numpy as np
 word_list = words.words()
 #word_list = ["A", "a", "aa", "aal", "aalii", "aam", "Aani", "b", "sdfg"]
 
 message = '''Ovg fh ivgfim uli z nlnvmg gl Ozwb Olevozxv’h lyqvxgrlm dsrxs hgzgvw gszg gsv nzxsrmv xzm lmob wl dszg dv gvoo rg gl wl'''
 #message = "ji he noem li gaiiag"
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" + "’"
+alphabet = "abcdefghijklmnopqrstuvwxyz" + "’"
 punctuation = "?!.,"
 sol_dict= {}
 length_dict = {}
@@ -23,8 +24,10 @@ def create_length_dict():
             length_dict[length] = [key]
     length_dict[1] = ["A", "a", "I", "i"]
 
-def initialize_sol():
+def initialize_sol(message):
     #sets each word in the message to a solution set equal to the word length
+    message = message.lower()
+    print(message)
     message_words = []
     word = ""
     i=1
@@ -39,15 +42,62 @@ def initialize_sol():
             message_words.append(word)
             word = ""
         i+=1
-
+    #print("list", message_words)
     for word in message_words:
     #matches each message word with potential solution list based on length
+        #input()
+        #print(word)
         length = len(word)
         sol_dict[word] = length_dict[length]
+        #remember this will get rid of duplicates
+        #print(sol_dict.keys())
 
 
+def pattern():
+#this function will go through
+    for word in sol_dict:
+
+        letter_freq = {}
+        letter_uni_loc = {}
+
+        for letter in word:
+        #creates dictionary showing frequency of each letter in word
+            if letter in letter_freq:
+                letter_freq[letter] += 1
+            else:
+                letter_freq[letter] = 1
+
+        for letter in list(letter_freq.keys()):
+        #creates dict recording unique position of replicated letters
+            replicate = []
+            if letter_freq[letter] >= 2:
+                for index in range(len(word)):
+                    if word[index] == letter:
+                        replicate.append(index)
+                    else:
+                        pass
+                letter_uni_loc[letter] = replicate
+
+
+
+        if len(letter_uni_loc) >= 1:
+            print("final", word, letter, letter_uni_loc)
+        else:
+            pass
+
+
+
+
+
+
+
+
+#Next steps:
+    #looking for similar symbols?
+    #
 
 
 
 create_length_dict()
-initialize_sol()
+initialize_sol(message)
+pattern()
