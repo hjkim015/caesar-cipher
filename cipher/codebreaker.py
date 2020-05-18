7095fe132caad57e738d899162d8243951ca8c26
Priscilla Wang <hjkim015@users.noreply.github.com>
2020-05-17 19:23:13 -0700

    finish duplicate function and begin codify function

--- cipher/codebreaker.py
+++ cipher/codebreaker.py
@@ -1,9 +1,9 @@
 
 from nltk.corpus import words
 import numpy as np
-word_list = words.words()
-#word_list = ["A", "a", "aa", "aal", "aalii", "aam", "Aani", "b", "sdfg"]
+import re
 
+word_list = words.words()
 message = '''Ovg fh ivgfim uli z nlnvmg gl Ozwb Olevozxv’h lyqvxgrlm dsrxs hgzgvw gszg gsv nzxsrmv xzm lmob wl dszg dv gvoo rg gl wl'''
 #message = "ji he noem li gaiiag"
 alphabet = "abcdefghijklmnopqrstuvwxyz" + "’"
@@ -11,93 +11,91 @@ punctuation = "?!.,"
 sol_dict= {}
 length_dict = {}
 
+
 def create_length_dict():
 #creates a dictionary that organizes English words based on their length
     for key in word_list:
-    #goes through every English word and finds their length
         length = len(key)
         if length in length_dict:
-        #if the word length is already in the new dict, just add the word
             length_dict[length].append(key)
         else:
-        #otherwise, create another key value pair
             length_dict[length] = [key]
     length_dict[1] = ["A", "a", "I", "i"]
 
 def initialize_sol(message):
-    #sets each word in the message to a solution set equal to the word length
+#sets each word in the message to a solution set equal to the word length
     message = message.lower()
-    print(message)
     message_words = []
     word = ""
     i=1
     for symbol in message:
-    #sort into a list of words in the message instead of letters
         if symbol in alphabet:
             word += symbol
             if i == len(message):
-                #gets the very last word since it doesn't end with a space
                 message_words.append(word)
         else:
             message_words.append(word)
             word = ""
         i+=1
-    #print("list", message_words)
     for word in message_words:
     #matches each message word with potential solution list based on length
-        #input()
-        #print(word)
         length = len(word)
         sol_dict[word] = length_dict[length]
-        #remember this will get rid of duplicates
-        #print(sol_dict.keys())
-
 
 def pattern():
-#this function will go through
+#removes solutions that don't fit duplicate pattern
     for word in sol_dict:
-
-        letter_freq = {}
-        letter_uni_loc = {}
-
-        for letter in word:
-        #creates dictionary showing frequency of each letter in word
-            if letter in letter_freq:
-                letter_freq[letter] += 1
-            else:
-                letter_freq[letter] = 1
-
-        for letter in list(letter_freq.keys()):
-        #creates dict recording unique position of replicated letters
-            replicate = []
-            if letter_freq[letter] >= 2:
-                for index in range(len(word)):
-                    if word[index] == letter:
-                        replicate.append(index)
-                    else:
-                        pass
-                letter_uni_loc[letter] = replicate
+        print(word)
+        find_duplicates(word)
+        input()
 
 
-
-        if len(letter_uni_loc) >= 1:
-            print("final", word, letter, letter_uni_loc)
-        else:
-            pass
-
+            # for i in range(len(duplicates)):
+            #     n = i+1
+            #     word = word.replace(duplicates[i],str(n))
+            #     print(word)
 
 
+    index_positions = {}
+
+def find_duplicates(word):
+#finds the indices of all letters in word
+    regexPattern = re.compile('[a-zA-Z0-9]')
+    iteratorOfMatchObs = regexPattern.finditer(word)
+    index_positions = {}
+
+    for matchObj in iteratorOfMatchObs:
+        index_positions[matchObj.group()] = index_positions.get(matchObj.group(), []) + [matchObj.start()]
+
+    print(index_positions.items())
+
+def codify(word,index_positions):
+#turns the encoded words and all potential solutions into number codes
+    zeros = word.replace(word, "0" * len(word))
+    print(index_positions)
+
+
+        #print(code)
+    # for (key, value) in index_positions.items():
+    #     if len(index_positions[key]) >= 2:
+    #        ß print("indices of ", key , " is : ", index_positions[key])
 
 
 
 
 
-#Next steps:
-    #looking for similar symbols?
-    #
+
+
+
+
+
+
+
 
 
 
 create_length_dict()
 initialize_sol(message)
 pattern()
+#find_duplicates("olevozxv’h")
+#codify("nlnvmg", index_positions)
