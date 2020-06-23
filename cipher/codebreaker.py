31f91a784643f53e2d870cad3c623800931dfad3
Priscilla Wang <hjkim015@users.noreply.github.com>
2020-06-23 14:39:22 -0700

    modified duplicates and cipher_test function

--- cipher/codebreaker.py
+++ cipher/codebreaker.py
@@ -6,7 +6,10 @@ import string
 import re
 
 english_words = words.words()
-message = '''Ovg fh ivgfim uli z nlnvmg gl Ozwb lyqvxgrlm dsrxs hgzgvw gszg gsv nzxsrmv xzm lmob wl dszg dv gvoo rg gl wl'''
+message = '''Ovg fh ivgfim uli z nlnvmg gl Ozwb Olevozxv’h lyqvxgrlm, dsrxs hgzgvw gszg gsv nzxsrmv xzm lmob wl dszg dv gvoo rg gl wl. Lmv xlfow hzb gszg z nzm xzm "rmqvxg" zm rwvz rmgl gsv nzxsrmv, zmw gszg rg droo ivhklmw gl z xvigzrm vcgvmg zmw gsvm wilk rmgl jfrvhxvmxv, orpv z krzml hgirmt hgifxp yb z sznnvi. Zmlgsvi hrnrov dlfow yv zm zglnrx krov lu ovhh gszm xirgrxzo hrav: zm rmqvxgvw rwvz rh gl xliivhklmw gl z mvfgilm vmgvirmt gsv krov uiln drgslfg. Vzxs hfxs mvfgilm droo xzfhv z xvigzrm wrhgfiyzmxv dsrxs vevmgfzoob wrvh zdzb. Ru, sldvevi, gsv hrav lu gsv krov rh hfuurxrvmgob rmxivzhvw, gsv wrhgfiyzmxv xzfhvw yb hfxs zm rmxlnrmt mvfgilm droo evib orpvob tl lm zmw lm rmxivzhrmt fmgro gsv dslov krov rh wvhgilbvw. Rh gsviv z xliivhklmwrmt ksvmlnvmlm uli nrmwh, zmw rh gsviv lmv uli nzxsrmvh? Gsviv wlvh hvvn gl yv lmv uli gsv sfnzm nrmw. Gsv nzqlirgb lu gsvn hvvn gl yv "hfy xirgrxzo," r.v. gl xliivhklmw rm gsrh zmzoltb gl krovh lu hfy-xirgrxzo hrav. Zm rwvz kivhvmgvw gl hfxs z nrmw droo lm zeviztv trev irhv gl ovhh gszm lmv rwvz rm ivkob. Z hnzoorhs kilkligrlm ziv hfkvixirgrxzo. Zm rwvz kivhvmgvw gl hfxs z nrmw nzb trev irhv gl z dslov "gsvlib" xlmhrhgrmt lu hvxlmwzib, gvigrzib zmw nliv ivnlgv rwvzh. Zmrnzoh’ nrmwh hvvn gl yv evib wvurmrgvob hfy-xirgrxzo. Zwsvirmt gl gsrh zmzoltb dv zhp,"Xzm z nzxsrmv yv nzwv gl yv hfkvi-xirgrxzo?'''
+
+#message = '''Ovg fh ivgfim uli z nlnvmg gl Ozwb Olevozxv’h lyqvxgrlm, dsrxs hgzgvw gszg gsv nzxsrmv xzm lmob wl dszg dv gvoo rg gl wl.'''
+
 alphabet = list(string.ascii_lowercase)
 contraction = " ' ’ "
 punctuation = "?!.,"
@@ -15,8 +18,9 @@ cipher = {}
 word_bank = {}
 
 def initialize_word_bank():
-#creates a dictionary that organizes English words based on their length
-# {2: "am", "is", etc.}
+    '''creates a dictionary that organizes English words based on their length.
+    All letters will be lowercase'''
+
     for word in english_words:
         length = len(word)
         if length in word_bank:
@@ -29,33 +33,45 @@ def initialize_word_bank():
 def initialize__sol(message):
 #initializes word_sol and cipher dictionaries
     message = message.lower()
+    message = ' '.join(word.strip(string.punctuation) for word in message.split())
+    #strips punctuation
     m_words = message.split()
+
     m_spaceless = list(message.replace(" ", ""))
     m_letters = list(dict.fromkeys(m_spaceless))
 
     for l in m_letters:
-        cipher[l] = alphabet
+        cipher[l] = alphabet[:]
     for word in m_words:
         length = len(word)
         if length == 1:
             cipher[word] = ["a", "i"]
-        word_sol[word] = word_bank[length]
+        word_sol[word] = word_bank[length].copy()
 
     return cipher, word_sol
 
 def duplicates():
-#removes solutions from word_sol that don't fit duplicate pattern
+    '''returns a modified word_sol dictionary that has removed solutions that
+    don't fit the duplicates pattern. For example, the word aal will be removed
+    from the the potential solutions for the encoded word ovg because ovg
+    does not have any duplicate letters like aal'''
+
     for word in word_sol:
+        # input("_____________________________________________________")
         duplicates = []
         for letter in word:
-            if word.count(letter) > 1 and letter not in duplicates:
-                duplicates.append(letter)
-        if len(duplicates) >= 1:
             code = codify(word)
             for sol in word_sol[word]:
+                before = len(word_sol[word])
                 test_code = codify(sol)
+                # print(word, sol)
+                # print(code, test_code)
                 if test_code != code:
                     word_sol[word].remove(sol)
+                    after = len(word_sol[word])
+                    # print(word, sol)
+                    # print("removed!", before, "-->", after)
+                    # print("ivgfim", len(word_sol["ivgfim"]))
 
     return word_sol
 
@@ -69,7 +85,7 @@ def find_indices(word):
     return index_positions
 
 def find_letter_indices(s, ch):
-#finds the index position of a specific letter in a word
+    '''returns a list of the index positions of a specific letter in a word'''
     indices = [i for i, ltr in enumerate(s) if ltr == ch]
     return indices
 
@@ -97,53 +113,71 @@ def codify(word):
     index_positions.clear()
     return zeros
 
-def one_letter():
-    for key, values in cipher.items():
-        length = len(cipher[key])
-        for value in values:
-            input()
-            print("the value we are on for the key: ", key, "is: ", value)
-            print("Before", length)
-            test_dict = word_sol.copy()
-            test_letter = value
-            encoded_letter = key
-            print(cipher[encoded_letter])
-            for word in test_dict:
-                input()
-                if encoded_letter in word:
-                    indices = find_letter_indices(word, encoded_letter)
-                    print("indices for encoded letter", encoded_letter, "in the word: ", word, indices)
-                    for sol in test_dict[word]:
-                        input()
-                        test_indices = find_letter_indices(sol, test_letter)
-                        print("indices for test letter ", test_letter, "in potential sol: ", sol, test_indices)
-                        if test_indices != indices:
-                            print("potential solutions length is before: ", len(test_dict[word]))
-                            test_dict[word].remove(sol)
-                            print("potential solutions length after: ", len(test_dict[word]))
-            if len(test_dict[word]) == 0:
+
+
+def one_letter_cipher_test(encoded_letter, test_letter):
+    '''takes a potential one_letter cipher mapping and returns a 1 or 0 whether the cipher
+    could be a solution for the message'''
+
+    test_dict = word_sol.copy()
+    results = []
+    for encoded_word in test_dict:
+        if encoded_letter in encoded_word:
+            result = cipher_word(encoded_letter, encoded_word, test_letter, test_dict)
+            results.append(result)
+
+    if 0 in results:
+        # print("not a potential cipher. please remove")
+        verdict = 0
+        return verdict
+    else:
+        verdict = 1
+        # print("a potential solution")
+        return verdict
+
+def cipher_word(encoded_letter, encoded_word, test_letter, test_dict):
+    '''This function is used for the _cipher_test function. It returns a 1 or 0 to show
+    whether a test_letter can be a solution for one encoded word'''
+    indices = find_letter_indices(encoded_word, encoded_letter)
+    # print("length --> of solutions for this word", len(test_dict[encoded_word]))
+    for sol in test_dict[encoded_word]:
+        if sol[indices[0]] == test_letter:
+            # print("sol", sol)
+            result = 1
+            return result
+
+    result = 0
+    return result
+
+
+def cipher_test():
+    '''will run through the whole entire cipher and will eliminate test_letters
+    that cannot be potential solutions for the message.'''
+
+    for encoded_letter, values in cipher.items():
+        print(encoded_letter, "values before", len(cipher[encoded_letter]))
+
+        for test_letter in values[::-1]:
+            # print("test_letter", test_letter)
+            result = one_letter_cipher_test(encoded_letter, test_letter)
+            if result == 0:
                 cipher[encoded_letter].remove(test_letter)
-                print("after", length)
+        print(encoded_letter, "after-->", len(cipher[encoded_letter]))
+        input("_________________________________________________")
+
+    return cipher
 
 
 
+'''action items:
+    1.) convert the english words to lowercase so they're easier to work with.
 
+'''
 
-
-
-#set single letter to either "a" or "i"
-#randomly assign cipher letters, if any of the words go empty, it can't english_words
-#test this like 25*25 times for every alphabet letter.
-#testing one at a time.
-
-#letter dict: key is message letter, key value is alphabet letter
-#assign the letter, and go through every encoded word that has that letter
-#if word bank empty eliminate.
 
 initialize_word_bank()
 initialize__sol(message)
 duplicates()
-one_letter()
+cipher_test()
 
-#pattern()
-#one_letter()
+#cipher_test()
